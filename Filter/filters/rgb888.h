11166e251b4a00a9638b7f1269cf2037591723df
Vitaly Grachev <v.grachev@g.nsu.ru>
2017-03-24 10:20:24 +0700

    Addded constructor from QColor.

--- Filter/filters/rgb888.h
+++ Filter/filters/rgb888.h
@@ -2,6 +2,7 @@
 #define RGB888_H
 
 #include <cstdint>
+#include <QColor>
 
 struct RGB888 {
     uint8_t r;
@@ -9,6 +10,8 @@ struct RGB888 {
     uint8_t b;
 
     RGB888() = default;
+
+    explicit RGB888(const QColor & color) : r(color.red()), g(color.green()), b(color.blue()) {}
 
     RGB888(uint8_t rc, uint8_t gc, uint8_t bc) : r(rc), g(gc), b(bc) {}
 
