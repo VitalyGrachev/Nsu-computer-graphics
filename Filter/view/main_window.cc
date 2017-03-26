36ab051c55e06f0567d9ea64514b4f1b59d21b40
Vitaly Grachev <v.grachev@g.nsu.ru>
2017-03-27 00:53:14 +0700

    Added zones

--- Filter/view/main_window.cc
+++ Filter/view/main_window.cc
@@ -1,1 +1,26 @@
 #include "main_window.h"
+#include <QScrollArea>
+#include <QHBoxLayout>
+
+MainWindow::MainWindow(QWidget * parent)
+        : QMainWindow(parent) {
+    QScrollArea * scroll_area = new QScrollArea(this);
+    QWidget * central_widget = new QWidget(this);
+    QHBoxLayout * central_layout = new QHBoxLayout(central_widget);
+
+    central_layout->addSpacing(zone_spacing);
+    central_layout->addWidget(zone_A = new Zone(zone_width, zone_height));
+    central_layout->addSpacing(zone_spacing);
+    central_layout->addWidget(zone_B = new Zone(zone_width, zone_height));
+    central_layout->addSpacing(zone_spacing);
+    central_layout->addWidget(zone_C = new Zone(zone_width, zone_height));
+    central_layout->addSpacing(zone_spacing);
+
+    scroll_area->setWidget(central_widget);
+    scroll_area->setAlignment(Qt::AlignCenter);
+
+    setCentralWidget(scroll_area);
+
+    setWindowTitle(tr("FIT_14202_Grachev_Filter"));
+    resize(800, 600);
+}
--- Filter/view/main_window.h
+++ Filter/view/main_window.h
@@ -2,10 +2,23 @@
 #define MAIN_WINDOW_H
 
 #include <QMainWindow>
+#include "zone.h"
+#include "../filter_applicator.h"
 
 class MainWindow : public QMainWindow {
 public:
+    MainWindow(QWidget * parent = nullptr);
 
+private:
+    Zone * zone_A;
+    Zone * zone_B;
+    Zone * zone_C;
+
+    FilterApplicator filter_applicator;
+
+    static const int zone_width = 350;
+    static const int zone_height = 350;
+    static const int zone_spacing = 10;
 };
 
 #endif //MAIN_WINDOW_H
