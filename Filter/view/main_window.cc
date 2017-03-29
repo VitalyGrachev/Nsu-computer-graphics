c78f9b11fd2896cf1b24e78db678b35349fee6ee
Vitaly Grachev <v.grachev@g.nsu.ru>
2017-03-29 22:02:30 +0700

    Added zones, menus. Implemented open, filter widget creation.

--- Filter/view/main_window.cc
+++ Filter/view/main_window.cc
@@ -1,15 +1,45 @@
 #include "main_window.h"
+
 #include <QScrollArea>
 #include <QHBoxLayout>
+#include <QAction>
+#include <QMenu>
+#include <QMenuBar>
+#include <QFileDialog>
+
+#include "../factory.h"
+
+const int MainWindow::zone_width = 350;
+const int MainWindow::zone_height = 350;
+const int MainWindow::zone_spacing = 10;
 
 MainWindow::MainWindow(QWidget * parent)
         : QMainWindow(parent) {
+    create_dock_widget();
+    create_central_widget();
+    create_menus_and_toolbar();
+
+    connect(zone_A, SIGNAL(selected_image(ImageWrapper)),
+            zone_B, SLOT(set_image(ImageWrapper)));
+
+    setWindowTitle(tr("FIT_14202_Grachev_Filter"));
+    resize(1200, 600);
+}
+
+void MainWindow::create_dock_widget() {
+    dock_widget = new QDockWidget(this);
+    dock_widget->setFloating(false);
+    dock_widget->setFeatures(QDockWidget::NoDockWidgetFeatures);
+    addDockWidget(Qt::BottomDockWidgetArea, dock_widget);
+}
+
+void MainWindow::create_central_widget() {
     QScrollArea * scroll_area = new QScrollArea(this);
     QWidget * central_widget = new QWidget(this);
     QHBoxLayout * central_layout = new QHBoxLayout(central_widget);
 
     central_layout->addSpacing(zone_spacing);
-    central_layout->addWidget(zone_A = new Zone(zone_width, zone_height));
+    central_layout->addWidget(zone_A = new ZoneA(zone_width, zone_height));
     central_layout->addSpacing(zone_spacing);
     central_layout->addWidget(zone_B = new Zone(zone_width, zone_height));
     central_layout->addSpacing(zone_spacing);
@@ -20,7 +50,61 @@ MainWindow::MainWindow(QWidget * parent)
     scroll_area->setAlignment(Qt::AlignCenter);
 
     setCentralWidget(scroll_area);
+}
 
-    setWindowTitle(tr("FIT_14202_Grachev_Filter"));
-    resize(800, 600);
+void MainWindow::create_menus_and_toolbar() {
+    QAction * new_action = new QAction(tr("&New"), this);
+    connect(new_action, SIGNAL(triggered()),
+            this, SLOT(clear()));
+
+    QAction * open_action = new QAction(tr("&Open"), this);
+    connect(open_action, SIGNAL(triggered()),
+            this, SLOT(open()));
+
+    QAction * save_action = new QAction(tr("&Save"), this);
+    connect(save_action, SIGNAL(triggered()),
+            this, SLOT(save()));
+
+    QMenu * file_menu = new QMenu(tr("&File"), this);
+    file_menu->addAction(new_action);
+    file_menu->addAction(open_action);
+    file_menu->addAction(save_action);
+
+    menuBar()->addMenu(file_menu);
+}
+
+void MainWindow::set_filter_widget(const QString & filter_name) {
+    filter_widget = Factory<std::string, BaseFilterWidget>::instance().create(filter_name.toStdString());
+    dock_widget->setWidget(filter_widget);
+    dock_widget->setWindowTitle(filter_name);
+}
+
+void MainWindow::clear() {
+    zone_A->clear();
+    zone_B->clear();
+    zone_C->clear();
+    has_something_to_save = false;
+    // TODO: disable filters
+}
+
+void MainWindow::open() {
+    QString filename = QFileDialog::getOpenFileName(this,
+                                                    tr("Open image"),
+                                                    "./",
+                                                    tr("Images (*.png *.bmp *.jpg);;All files(*.*)"));
+    QImage image;
+    if (!filename.isNull() && image.load(filename)) {
+        image.convertToFormat(QImage::Format_ARGB32);
+        zone_A->set_image(image);
+        zone_B->clear();
+        zone_C->clear();
+
+        zone_A->set_selection_enabled(true);
+    } else {
+        // TODO: show error message
+    }
+}
+
+void MainWindow::save() {
+
 }
--- Filter/view/main_window.h
+++ Filter/view/main_window.h
@@ -2,23 +2,49 @@
 #define MAIN_WINDOW_H
 
 #include <QMainWindow>
+#include <QDockWidget>
 #include "zone.h"
+#include "zone_a.h"
 #include "../filter_applicator.h"
+#include "../filters/base_filter_widget.h"
 
 class MainWindow : public QMainWindow {
+Q_OBJECT
 public:
     MainWindow(QWidget * parent = nullptr);
 
+private slots:
+
+    void set_filter_widget(const QString & filter_name);
+
+    void clear();
+
+    void open();
+
+    void save();
+
 private:
-    Zone * zone_A;
+    void create_dock_widget();
+
+    void create_central_widget();
+
+    void create_menus_and_toolbar();
+
+    ZoneA * zone_A;
     Zone * zone_B;
     Zone * zone_C;
 
+    QDockWidget * dock_widget;
+
+    BaseFilterWidget * filter_widget;
+
     FilterApplicator filter_applicator;
 
-    static const int zone_width = 350;
-    static const int zone_height = 350;
-    static const int zone_spacing = 10;
+    bool has_something_to_save;
+
+    static const int zone_width;
+    static const int zone_height;
+    static const int zone_spacing;
 };
 
 #endif //MAIN_WINDOW_H
