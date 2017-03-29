545bb840de424ca8ee656e8a306c998ac42bd1fa
Vitaly Grachev <v.grachev@g.nsu.ru>
2017-03-29 21:58:58 +0700

    Added convolution filter.

---
+++ Filter/filters/convolution/convolution_filter.h
@@ -0,0 +1,64 @@
+#ifndef CONVOLUTION_FILTER_H
+#define CONVOLUTION_FILTER_H
+
+#include "../abstract_filter.h"
+#include "square_matrix.h"
+
+template<class T>
+class ConvolutionFilter : public AbstractFilter {
+public:
+    ConvolutionFilter(const SquareMatrix<T> & kernel);
+
+    ImageWrapper operator()(const ImageWrapper & input) override;
+
+private:
+    SquareMatrix<T> kernel;
+};
+
+template<class T>
+ConvolutionFilter<T>::ConvolutionFilter(const SquareMatrix<T> & kernel)
+        : kernel(kernel) {}
+
+namespace {
+int clamp(int value, int min, int max) {
+    return std::min(std::max(value, min), max);
+}
+}
+
+template<class T>
+ImageWrapper ConvolutionFilter<T>::operator()(const ImageWrapper & input) {
+    const int width = input.width();
+    const int height = input.height();
+    const int k_half_size = kernel.size() / 2;
+    const T zero = static_cast<T>(0);
+    ImageWrapper output(width, height);
+
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            T val_r = zero;
+            T val_g = zero;
+            T val_b = zero;
+            for (int ky = 0; ky < kernel.size(); ++ky) {
+                for (int kx = 0; kx < kernel.size(); ++kx) {
+                    const int px = clamp(x + kx - k_half_size, 0, width);
+                    const int py = clamp(y + ky - k_half_size, 0, height);
+                    const RGBA32 in = input(px, py);
+                    const T & k_value = kernel(kx, ky);
+                    val_r += k_value * in.ch.r;
+                    val_g += k_value * in.ch.g;
+                    val_b += k_value * in.ch.b;
+                }
+            }
+            const RGBA32 anchor = input(x, y);
+            RGBA32 out(anchor.ch.a,
+                       static_cast<uint8_t>(val_r),
+                       static_cast<uint8_t>(val_g),
+                       static_cast<uint8_t>(val_b));
+            input(x, y) = out.qrgb;
+        }
+    }
+    return output;
+}
+
+
+#endif //CONVOLUTION_FILTER_H
---
+++ Filter/filters/convolution/square_matrix.h
@@ -0,0 +1,77 @@
+#ifndef SQUARE_MATRIX_H
+#define SQUARE_MATRIX_H
+
+#include <vector>
+#include <stdexcept>
+
+template<class T>
+class SquareMatrix {
+public:
+    static SquareMatrix eye(int odd_size);
+
+    static SquareMatrix zeros(int odd_size);
+
+    static SquareMatrix ones(int odd_size);
+
+    SquareMatrix(int odd_size, std::initializer_list<T> il);
+
+    SquareMatrix(const SquareMatrix &) = default;
+
+    SquareMatrix(SquareMatrix &&) = default;
+
+    SquareMatrix & operator=(const SquareMatrix &) = default;
+
+    SquareMatrix & operator=(SquareMatrix &&) = default;
+
+    T & operator()(int x, int y) { return values[y * size_ + x]; }
+
+    const T & operator()(int x, int y) const { return values[y * size_ + x]; }
+
+    int size() const { return size_; }
+
+private:
+    SquareMatrix(int odd_size, const T & fill_value);
+
+    std::vector<T> values;
+    int size_;
+};
+
+template<class T>
+SquareMatrix<T> SquareMatrix<T>::eye(int odd_size) {
+    SquareMatrix matrix(odd_size, static_cast<T>(0));
+    for (int i = 0; i < odd_size; ++i) {
+        matrix(i, i) = static_cast<T>(1);
+    }
+    return matrix;
+}
+
+template<class T>
+SquareMatrix<T> SquareMatrix<T>::zeros(int odd_size) {
+    return SquareMatrix(odd_size, static_cast<T>(0));
+}
+
+template<class T>
+SquareMatrix<T> SquareMatrix<T>::ones(int odd_size) {
+    return SquareMatrix(odd_size, static_cast<T>(1));
+}
+
+template<class T>
+SquareMatrix<T>::SquareMatrix(int odd_size, const T & fill_value)
+        : values(odd_size, fill_value), size_(odd_size) {
+    if (!odd_size % 2) {
+        throw std::invalid_argument("Size must be an odd number.");
+    }
+}
+
+template<class T>
+SquareMatrix<T>::SquareMatrix(int odd_size, std::initializer_list<T> il)
+        : values(il), size_(odd_size) {
+    if (il.size() != odd_size * odd_size) {
+        throw std::invalid_argument("Initializer list size_ doesn't match matrix size.");
+    }
+    if (!odd_size % 2) {
+        throw std::invalid_argument("Size must be an odd number.");
+    }
+}
+
+#endif //SQUARE_MATRIX_H
