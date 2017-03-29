50f37d453831fd00ae881e8dc66dc28add3011a9
Vitaly Grachev <v.grachev@g.nsu.ru>
2017-03-29 21:57:58 +0700

    Added ZoneA class, implemented roi selection.

---
+++ Filter/view/zone_a.cc
@@ -0,0 +1,97 @@
+#include "zone_a.h"
+
+#include <QMouseEvent>
+
+#include "../filters/scaling/scaling_filter.h"
+
+const QRgb ZoneA::rect_color = QColor(200, 200, 200).rgb();
+
+ZoneA::ZoneA(int width, int height, QWidget * parent)
+        : Zone(width, height, parent),
+          is_selecting(false),
+          is_selection_enabled(false) {}
+
+QRect ZoneA::selected_rect(const QPoint & mouse_pos) const {
+    const float scale = 1.0f / scale_factor;
+    const int pos_x = scale * (mouse_pos.x() - 1) - width() / 2;
+    const int pos_y = scale * (mouse_pos.y() - 1) - height() / 2;
+    const int rect_left_x = std::min(std::max(pos_x, 0), original_image.width() - width());
+    const int rect_top_y = std::min(std::max(pos_y, 0), original_image.height() - height());
+    const int rect_width = std::min(width(), original_image.width() - rect_left_x);
+    const int rect_height = std::min(height(), original_image.height() - rect_top_y);
+    return QRect(rect_left_x, rect_top_y, rect_width, rect_height);
+}
+
+void ZoneA::show_rect(const QRect & rect) {
+    ImageWrapper modified(scaled_image);
+    const int lx = rect.x();
+    const int ty = rect.y();
+    const int rx = lx + rect.width();
+    const int by = ty + rect.height();
+    modified.draw_line(lx, ty, rx, ty, rect_color);
+    modified.draw_line(lx, ty, lx, by, rect_color);
+    modified.draw_line(rx, ty, rx, by, rect_color);
+    modified.draw_line(lx, by, rx, by, rect_color);
+
+    Zone::set_image(modified);
+}
+
+void ZoneA::select_image(const QPoint & mouse_pos) {
+    QRect rect = selected_rect(mouse_pos);
+    emit selected_image(original_image.copy(rect));
+    show_rect(QRect(rect.x() * scale_factor, rect.y() * scale_factor,
+                    rect.width() * scale_factor, rect.height() * scale_factor));
+}
+
+void ZoneA::mousePressEvent(QMouseEvent * event) {
+    if (is_selection_enabled && event->button() == Qt::LeftButton) {
+        is_selecting = true;
+        select_image(event->pos());
+    }
+    event->accept();
+}
+
+void ZoneA::mouseMoveEvent(QMouseEvent * event) {
+    if (is_selecting) {
+        select_image(event->pos());
+    }
+    event->accept();
+}
+
+void ZoneA::mouseReleaseEvent(QMouseEvent * event) {
+    if (is_selecting && event->button() == Qt::LeftButton) {
+        is_selecting = false;
+        Zone::set_image(scaled_image);
+    }
+    event->accept();
+}
+
+void ZoneA::clear() {
+    Zone::clear();
+    original_image = ImageWrapper();
+}
+
+void ZoneA::set_image(ImageWrapper image) {
+    original_image = image;
+
+    const float w_scale_factor = static_cast<float>(width()) / image.width();
+    const float h_scale_factor = static_cast<float>(height()) / image.height();
+    const float res_scale_factor = std::min(w_scale_factor, h_scale_factor);
+    if (res_scale_factor < 1.0f) {
+        const int w = std::min(width(), static_cast<int>(res_scale_factor * image.width()));
+        const int h = std::min(height(), static_cast<int>(res_scale_factor * image.height()));
+        scale_factor = res_scale_factor;
+        ScalingFilter filter(w, h);
+        scaled_image = filter(image);
+        Zone::set_image(scaled_image);
+    } else {
+        scale_factor = 1.0f;
+        scaled_image = image;
+        Zone::set_image(image);
+    }
+}
+
+void ZoneA::set_selection_enabled(bool enabled) {
+    is_selection_enabled = enabled;
+    is_selecting = false;
+}
